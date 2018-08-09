83702b18427038c661d359d605b2ddccffdfb6e7
Kyle Kim <kyle.kim@amwestfunding.com>
2018-08-08 17:13:21 -0700

    PG3_Logger can play as basic

---
+++ PG3_Logger/Logging/Log.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging
+{
+    public class Log
+    {
+        public enum LogLevel
+        {
+            Error,
+            Warning,
+            Info,
+            Debug
+        }
+
+        public Log(LogLevel logType, DateTime logTime, string logMessage)
+        {
+            this.LogType = logType;
+            this.LogTime = logTime;
+            this.LogMessage = logMessage;
+        }
+
+        protected LogLevel LogType { get; set; }
+        protected DateTime LogTime { get; set; }
+        protected string LogMessage { get; set; }
+    }
+}
