ec9b59ca3239ec0418c9b61b7a2351e729e19f96
Kyle Kim <zzangqqrr@gmail.com>
2020-01-16 22:20:21 -0800

    Added Dynamic Array

--- DataStructureTestConsole/Program.cs
+++ DataStructureTestConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataStructure.Array;
+using System;
 
 namespace DataStructureTestConsole
 {
@@ -6,7 +7,21 @@ namespace DataStructureTestConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RunDynamicArray();
+        }
+
+        static void RunDynamicArray()
+        {
+            DynamicArray dynamicArray = new DynamicArray();
+            for (int i = 0; i < 20; i++)
+            {
+                dynamicArray.Add($"element{i + 1}");
+            }
+
+            for (int i = 0; i < dynamicArray.Count; i++)
+            {
+                Console.WriteLine(dynamicArray[i].ToString());
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -4,11 +4,13 @@ Goal
 * It is a project to train the development skills necessary for practice through various examples.
 
 Data structures
- * 
-    - 
+ * Array
+   - 
+ * Linked List
+   - 
  * Sorting
-    - 
+   - 
  * Searching
-    - 
+   - 
 
 Algorithm
