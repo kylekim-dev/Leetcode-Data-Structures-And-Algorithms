81937dd6150b9ea52ca3257fe25265bef51fec88
Kyle Kim <kyle.kim0923@gmail.com>
2023-01-31 15:43:32 -0800

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void run(int N, int X, int[] nums){
+        StringBuilder sb = new StringBuilder();
+
+        for (int n : nums) {
+            if(n < X){
+                sb.append(n + " ");
+            }
+        }
+
+        System.out.print(sb);
+    }
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\");
+        sb.append("\n");
+        sb.append(" )  ( ')");
+        sb.append("\n");
+        sb.append("(  /  )");
+        sb.append("\n");
+        sb.append(" \\(__)|");
+        System.out.print(sb);
+        /*try {
+            BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int N = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken());
+            int[] nums = new int[N];
+
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++){
+                nums[i] = Integer.parseInt(st.nextToken());
+            }
+
+            run(N, X, nums);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }*/
+    }
+}
