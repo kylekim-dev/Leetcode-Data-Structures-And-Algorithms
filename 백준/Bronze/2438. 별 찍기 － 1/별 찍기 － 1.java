1d3ae1eaed1c8d92253947d067327b34ae3d88b9
Kyle Kim <kyle.kim0923@gmail.com>
2023-02-01 09:40:55 -0800

    [Bronze V] Title: 별 찍기 - 1, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            StringBuilder sb = new StringBuilder();
+
+            for(int i = 1; i <= N; i++){
+                sb.append("*".repeat(i));
+                if(i < N){
+                    sb.append("\n");
+                }
+            }
+
+            System.out.print(sb);
+        }
+        catch (IOException ex){
+
+        }
+    }
+}
