5e4904b1014dfec61f409c2999f925327fcf3cfd
Kyle Kim <kyle.kim0923@gmail.com>
2023-04-05 15:36:07 -0700

    Update N과 M （1）.java

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,48 +1,47 @@
-/*
-    1. 아이디어
-        - Backtracking
-        - 재귀함수 안에서 for 돌면서 숫자 선택(방문 체크)
-        - 재귀함수에서 M개를 선택할 경우 print해주기
-    2. 시간복잡도
-        - N! (N < 10 가능)
-    3. 자료구조
-        - 결과값 저장 int[]
-        - 방문여부 체크  bool[]
- */
-
 import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int m;
-    static int n;
-    static void recursion(boolean[] visit, int[] nums, int depth){
-        if (m == depth) {
+    /*
+    1. 아이디어
+        - 백트래킹
+    2. 시간복잡도
+        - N! = 8! < 10 ^ 8 < 1s
+    3. 자료구조 & 알고리즘
+        - Backtracking
+    */
+    static int N, M;
+    static int[] nums;
+    static boolean[] visited;
+    static StringBuilder output = new StringBuilder();
+    public static void backtracking(int depth){
+        if (M == depth) {
             for(int n : nums) {
-                System.out.print(n + " ");
+                output.append(n).append(" ");
             }
-            System.out.println();
+            output.append("\n");
             return;
         }
 
-        for (int i = 1; i <= n; i++){
-            if (visit[i] == false){
+        for (int i = 1; i <= N; i++){
+            if (!visited[i]){
                 nums[depth] = i;
-                visit[i] = true;
-                recursion(visit, nums, depth + 1);
-                visit[i] = false;
+                visited[i] = true;
+                backtracking(depth + 1);
+                visited[i] = false;
             }
         }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        boolean[] visit = new boolean[n + 1];
-        int[] nums = new int[m];
+        visited = new boolean[N + 1];
+        nums = new int[M];
 
-        recursion(visit, nums, 0);
+        backtracking(0);
+        System.out.print(output);
     }
 }
