09ddc6148ea01cf58c30fe6d8d35734dea8273b2
Kyle Kim <kyle.kim0923@gmail.com>
2023-04-18 10:36:25 -0700

    Time: 38 ms (31.55%), Space: 42.9 MB (86.49%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,4 +1,21 @@
 class Solution {
+    /*
+        아이디어
+            1. 최단 거리를 위한 배열 min을 선언한다.
+            2. r = nums.length - 1;
+            3. 가장 끝 배열을 1로 초기화 한다.
+            4. r를 1 감소 시키고,
+                nums[1 + r, ... , nums[r] + r] 중에 가장 작은 값을 찾는다.
+            5. min[r] = 가장 작은 값 + 1을 해준다.
+            6. r > -1면 3 - 5번을 반복한다.
+            7. return min[0]미국 부채 한도 연장 6월
+
+        알고리즘
+            DP
+
+        시간 복잡도
+            O(N^2)
+     */
     public int jump(int[] nums) {
         int[] min = new int[nums.length];
         int r = nums.length - 1;
