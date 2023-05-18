174a8008843ccc58819658eb62c80734c38a4fb4
Kyle Kim <kyle.kim0923@gmail.com>
2023-05-18 09:54:24 -0700

    Time: 7 ms (55.58%), Space: 53.3 MB (5.64%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,9 +1,23 @@
 class Solution {
-    /* 
-        Time: O(N)
-        Space: O(N)
-    */
     public List<Integer> findDuplicates(int[] nums) {
+        //  Constraints: 1 <= n <= 10^5
+        //  Time: O(N)
+        //  Space: O(1)
+        List<Integer> result = new ArrayList<>();
+        
+        for(int n : nums){
+            n = Math.abs(n);
+            if(nums[n - 1] < 0) result.add(n);
+            else nums[n - 1] *= -1;
+        }
+        
+        return result;
+    }
+    
+    /*public List<Integer> findDuplicates(int[] nums) {
+        //  Brute-Force
+        //  Time: O(N)
+        //  Space: O(N)
         List<Integer> result = new ArrayList<>();
         HashSet<Integer> set = new HashSet<>();
         
@@ -13,5 +27,5 @@ class Solution {
         }
         
         return result;
-    }
+    }*/
 }
