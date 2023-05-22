69b30fee6667912f79ab72ff644106d3c71d2455
Kyle Kim <kyle.kim0923@gmail.com>
2023-05-22 16:44:20 -0700

    Time: 13 ms (33.83%), Space: 44 MB (6.58%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,41 +1,20 @@
 class Solution {
-    /*
-        row:
-            { 1, [5, 3, 7] }
-            { 2, [6, 1, 9, 5] }
-        col:
-            { 1, [5, 6, 8, 4, 7] }
-        rec:
-            { 0, [5, 6, 9, 8] }
-    
-    */
     public boolean isValidSudoku(char[][] board) {
-        HashMap<Integer, HashSet<Integer>> row = new HashMap<>();
-        HashMap<Integer, HashSet<Integer>> col = new HashMap<>();
-        HashMap<Integer, HashSet<Integer>> sq = new HashMap<>();
-        int key = 0;
-        int sqIndex = 0;
-        
-        for(int i = 0; i < 9; i++){
-            row.put(i, new HashSet<>());
-            col.put(i, new HashSet<>());
-            sq.put(i, new HashSet<>());
-        }
+        HashSet<String> set = new HashSet<>();
+        int section = 0;
         
         for(int r = 0; r < board.length; r++){
             for(int c = 0; c < board[r].length; c++){
-                
                 if(board[r][c] != '.'){
-                    key = board[r][c];
-                    sqIndex = (r / 3) * 3 + (c / 3);
+                    String row = "r" + r + board[r][c];
+                    String col = "c" + c + board[r][c];
+                    String sq = "s" + (r / 3) * 3 + (c / 3) + board[r][c];
                     
-                    if(row.get(r).contains(key) || col.get(c).contains(key) || sq.get(sqIndex).contains(key)){
-                        return false;
-                    }
+                    if(set.contains(row) || set.contains(col) || set.contains(sq)) return false;
                     
-                    row.get(r).add(key); 
-                    col.get(c).add(key); 
-                    sq.get(sqIndex).add(key);
+                    set.add(row);
+                    set.add(col);
+                    set.add(sq);
                 }
             }
         }
