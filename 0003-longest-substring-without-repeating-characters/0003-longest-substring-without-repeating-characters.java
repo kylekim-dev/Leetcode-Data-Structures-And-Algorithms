70820048a42a26d5efbb68618e247519423ab0f9
Kyle Kim <kyle.kim0923@gmail.com>
2023-05-26 15:42:09 -0700

    Time: 3 ms (96.08%), Space: 42.1 MB (99.45%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -4,6 +4,29 @@ class Solution {
         // Time: O(N), Space: O(N)
         int longest = 0;
         int l = 0, r = 0;
+        boolean[] set = new boolean[256];
+        
+        while(r < s.length()){
+            if(set[s.charAt(r)]){
+                set[s.charAt(l)] = false;
+                l += 1;
+            }
+            else {
+                set[s.charAt(r)] = true;
+                longest = Math.max(longest, r - l  + 1);
+                r += 1;
+            }
+        }
+        
+        return longest;
+    }
+    
+    /*
+    public int lengthOfLongestSubstring(String s) {
+        // Algorithm: Two Pointer, HashSet
+        // Time: O(N), Space: O(N)
+        int longest = 0;
+        int l = 0, r = 0;
         HashSet<Integer> set = new HashSet<>();
         
         while(r < s.length()){
@@ -19,5 +42,5 @@ class Solution {
         }
         
         return longest;
-    }
+    }*/
 }
