5cf2b5439d14e703dbbcc173f8d58b7148f3b72e
Kyle Kim <kyle.kim0923@gmail.com>
2023-05-30 10:06:51 -0700

    Time: 0 ms (100.00%), Space: 39.6 MB (45.51%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -2,6 +2,22 @@ class Solution {
     public int fib(int n) {
         // Algorithm: DP Memoization (Bottom-Up)
         // Time: O(N), Space: O(N)
+        if (n < 2) return n;
+        
+        int a = 0, b = 1;
+        int temp;
+        
+        for(int i = 2; i <= n; i++){
+            temp = b;
+            b = a + b;
+            a = temp;
+        }
+        
+        return b;
+    }
+    /*public int fib(int n) {
+        // Algorithm: DP Memoization (Bottom-Up)
+        // Time: O(N), Space: O(N)
         if(n < 1) return 0;
         
         int[] dp = new int[n + 1];
@@ -12,7 +28,7 @@ class Solution {
             dp[i] = dp[i - 1] + dp[i - 2];
         
         return dp[n];
-    }
+    }*/
     
     
     /*public int fib(int n) {
