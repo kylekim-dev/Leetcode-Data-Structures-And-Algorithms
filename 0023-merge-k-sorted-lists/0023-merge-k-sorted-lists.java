f0917a6bc8832cbfd91a62dabeed53ce96035645
Kyle Kim <kyle.kim0923@gmail.com>
2023-05-31 14:47:22 -0700

    Time: 5 ms (77.57%), Space: 43.9 MB (78.01%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    /*
+        
+        1 > 1 > 2 > 3 > 4 > 4 > 5 > 6
+    
+    */
+    public ListNode mergeKLists(ListNode[] lists) {
+        // Algorithm: Min Heap
+        // K = 10^4, N = 500
+        // Time: O(N*K*LogK), Space: O(K)
+        
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        ListNode dummy = new ListNode();
+        ListNode node = dummy;
+        
+        for(ListNode n : lists){
+            if(n != null) pq.offer(n);
+        }
+        
+        while(!pq.isEmpty()){
+            ListNode curr = pq.poll();
+            node.next = new ListNode(curr.val);
+            node = node.next;
+            
+            if(curr.next != null) pq.offer(curr.next);
+        }
+        
+        return dummy.next;
+    }
+}
