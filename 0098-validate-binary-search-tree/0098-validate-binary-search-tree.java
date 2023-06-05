c08ca7e1bb63175a10d7c7d943cbe7a9aca6622a
Kyle Kim <kyle.kim0923@gmail.com>
2023-06-05 16:56:05 -0700

    Time: 0 ms (100.00%), Space: 44.1 MB (12.54%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /*
+        Edge Case:
+        root = [100, 30, 150, null, null, 80, 200]
+                100
+               /   \
+              30   150
+                  /  \  
+                 80  200
+    
+            5
+            
+         4     6
+         
+             3    7
+    */
+    
+    TreeNode prev;
+
+    public boolean isValidBST(TreeNode root) {
+        // Algorithm: Recursion and Inorder
+        // Time: O(N), Space: O(1)
+        if(root == null) return true;
+        
+        boolean a = isValidBST(root.left);
+        
+        if(prev != null && prev.val >= root.val) return false;
+        else prev = root;
+        
+        a = a && isValidBST(root.right);
+        
+        return a;
+    }
+}
