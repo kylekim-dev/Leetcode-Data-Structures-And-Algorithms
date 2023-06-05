8d4ac9219e3cd22c6be228fab1569568ce56729f
Kyle Kim <kyle.kim0923@gmail.com>
2023-06-05 15:50:08 -0700

    Time: 2 ms (89.86%), Space: 40.6 MB (71.78%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,5 +1,7 @@
 class Solution {
     public boolean isValid(String s) {
+        // Algorithm: Stack, Iterator
+        // Time: O(N), Space: O(N)
         Stack<Character> stack = new Stack<>();
         
         for(Character c : s.toCharArray()){
