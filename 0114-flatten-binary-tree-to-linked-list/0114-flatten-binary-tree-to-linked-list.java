11e780b7583067f09dd053c7eb7cc714b205ba8a
Kyle Kim <kyle.kim0923@gmail.com>
2023-06-08 20:44:26 -0700

    Time: 0 ms (100.00%), Space: 41 MB (94.13%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.java
@@ -18,17 +18,19 @@ class Solution {
     TreeNode end = null;
     
     public void flatten(TreeNode root) {
+        // Algorithm: Inorder, Recursion
+        // Time: O(N), Space: O(N) << Stack 영역, O(1) << Heap 영역
         if(root == null) return;
         
         flatten(root.left);
+        
         if(root.left != null){
             end.right = root.right;
-            //end = root.right;
             root.right = root.left;
             root.left = null;
         }
-        flatten(root.right);
         
+        flatten(root.right);
         if(root.right == null) end = root;
     }
 }
