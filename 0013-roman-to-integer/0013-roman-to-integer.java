b533449c7f117124d84a920e14ad0261edcadfb4
Kyle Kim <kyle.kim0923@gmail.com>
2023-06-12 11:37:24 -0700

    Time: 6 ms (50.90%), Space: 43.6 MB (40.90%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -0,0 +1,42 @@
+class Solution {
+    /*
+        I
+        II
+        III or IIV
+        IV
+        V
+        
+        
+        MCMXCIV
+        
+        M:      1000
+        MC:     1000 + 100
+        MCM:    1000 - 100 + 1000
+        MCMX:   1000 - 100 + 1000 + 10
+        MCMXC:   1000 - 100 + 1000 - 10 + 100
+        MCMXCI:   1000 - 100 + 1000 - 10 + 100 + 1
+        MCMXCIV:   1000 - 100 + 1000 - 10 + 100 - 1 + 5
+        
+    */
+    
+    public int romanToInt(String s) {
+        int num = 0, prev = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        for(Character c : s.toCharArray()){
+            if(map.get(c) > prev) num -= prev;
+            else num += prev;
+            
+            prev = map.get(c);
+        }
+        
+        return num + prev;
+    }
+}
