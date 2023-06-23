770a8767aa56964521668f2529da5bd275fdc088
Kyle Kim <kyle.kim0923@gmail.com>
2023-06-23 13:21:08 -0700

    Time: 17 ms (23.78%), Space: 47.3 MB (79.77%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,28 +1,33 @@
 class Solution {
-    /*
-        [1,1,1,3,2,2,3,3]
-        
-        1, 3
-        
-    */
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        PriorityQueue<Integer[]> pq = new PriorityQueue<>((o1, o2) -> o2[1].compareTo(o1[1]));
-        // O(N)
-        for (int n: nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
-        }
-
-        for (int key:map.keySet()) {
-            pq.offer(new Integer[]{key, map.get(key)});
-        }
-
-        int[] result = new int[k];
-
-        for(int i = 0; i < k; i++){
-            result[i] = pq.poll()[0];
+        HashMap<Integer, Integer> count = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        int[] output = new int[k];
+        
+        for(int num : nums){
+            count.put(num, count.getOrDefault(num, 0) + 1); //{ 1:3, 2:2, 3:1 }
+            max = Math.max(max, count.get(num));
+        } 
+        
+        List<List<Integer>> bucketSort = new ArrayList<>();
+        
+        for(int i = 0; i <= max; i++) bucketSort.add(new ArrayList<>());
+        
+        count.forEach((key, value) -> {
+            List<Integer> list = bucketSort.get(value);
+            list.add(key);
+        });
+        
+        
+        for(int i = bucketSort.size() - 1; i >= 0; i--){
+            List<Integer> list = bucketSort.get(i);
+            
+            for(int num : list){
+                output[--k] = num;
+                if(k <= 0) return output;
+            }
         }
         
-        return result;
+        return output;
     }
 }
