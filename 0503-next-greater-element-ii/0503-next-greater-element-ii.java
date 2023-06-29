574cb9f39ec813e31e6f12f304c6b614ec581dab
Kyle Kim <kyle.kim0923@gmail.com>
2023-06-29 14:26:25 -0700

    Time: 43 ms (26.01%), Space: 44.9 MB (40.88%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -28,17 +28,15 @@ class Solution {
         [3,1,2,1,4,1]
         [4,2,4,4,-1,3]
     */
-    // Monotonic Stack
-    // O(N) O(N)
+    // Algorithm: Monotonic Stack
+    // Time: O(N), Space: O(N)
     public int[] nextGreaterElements(int[] nums) {
         int[] output = new int[nums.length];
         Stack<Integer> s = new Stack<>();
         
         for(int c = 0; c < 2; c++){
             for(int i = nums.length - 1; i >=0; i--){
-                while(!s.isEmpty() && s.peek() <= nums[i]){
-                    s.pop();
-                } 
+                while(!s.isEmpty() && s.peek() <= nums[i]) s.pop();
 
                 output[i] = s.isEmpty() ? -1 : s.peek();
                 s.push(nums[i]);
