c8c27194bb64b56d9f4b817081c749d6608eb31a
Kyle Kim <kyle.kim0923@gmail.com>
2023-07-03 15:43:42 -0700

    Time: 3 ms (52.54%), Space: 43.8 MB (46.82%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.java
@@ -1,32 +1,17 @@
 class Solution {
     /*
-        [8,4,6,2,3]
-        
-        4,2,4,2,3
-    
-        [8,4,6,4,5,2]
-        
-        4 6 4
-        
-        ans: 4
-        
-        ans: 4 0 2 2 3 2
-            
+        Algorithm: Monotonic Stack
+        Time: O(N), Space: O(N)
     */
     public int[] finalPrices(int[] prices) {
-        int[] ans = new int[prices.length];
-        Stack<Integer[]> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         
-        for(int i = 0; i < prices.length; i++){
-            ans[i] = prices[i];
-            while(!stack.isEmpty() && stack.peek()[0] >= prices[i]){
-                Integer[] temp = stack.pop();
-                ans[temp[1]] = temp[0] - prices[i];
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[i] <= prices[stack.peek()]) {
+                prices[stack.pop()] -= prices[i];
             }
-            
-            stack.push(new Integer[]{prices[i], i});
+            stack.push(i);
         }
-        
-        return ans;
+        return prices;
     }
 }
