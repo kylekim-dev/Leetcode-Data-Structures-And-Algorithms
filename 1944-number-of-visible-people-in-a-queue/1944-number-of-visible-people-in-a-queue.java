7d3d1205c3fa89b00fdd77caa98b7c5dd4289db6
Kyle Kim <kyle.kim0923@gmail.com>
2023-07-05 11:48:36 -0700

    Time: 125 ms (68.10%), Space: 62.2 MB (6.44%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.java
@@ -9,8 +9,9 @@ class Solution {
         전략
         1. 배열 heights의 끝부분부터 탐색을 시작한다.
         2. output[i] = stack.size()
-        3. 현재의 높이 h[i] > stack.peek() stack의 값을 pop해주고 popCount 1증가시켜준다.. (stack이 비어있으면 비교를 멈춘다)
-        4. i >= 0면 2,3번을 반복한다.
+        3. 현재의 높이 h[i] > stack.peek() stack의 값을 pop해주고 popCount 1증가시켜준다. (stack이 비어있으면 비교를 멈춘다)
+        4. 3번을 완료하면 자신보다 작은 기둥들을 모두 제거한 상태, 만약 Stack이 비어있지 않으면 자신보다 큰 사람이 있으므로 1을 증가시켜준다.
+        5. Stack에 h[i]를 넣어준뒤, i >= 0면 2,3번을 반복한다.
         
         
         [10,6,1,5,11,9]
