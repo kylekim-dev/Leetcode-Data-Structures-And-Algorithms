664a19a380b81eb4129ed6340c1e3eabef4617fa
Kyle Kim <kyle.kim0923@gmail.com>
2023-07-10 16:02:57 -0700

    Time: 9 ms (75.60%), Space: 44.1 MB (49.68%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,16 +1,27 @@
 class Solution {
     /*
-    
+        Test Case:
         2 -5 5 10 -5
+        posi 5 10
+        nega -5
+        ans: -5 5 10
         
-        5 10
-        -5
+        Algorithm & DS: Stack
+        Time: O(N), Space: O(N)
         
-        -5 5 10
+        전략:
+        1. 배열의 끝부터 탐색을 시작한다.
+        2. 만약 현재 운석이 음수이면 stack에 모아준다.
+        3. 만약 양수 운석이면 음수 stack에 운석과 충돌시키고 현재 운석보다 작은 운석들을 스택에서 꺼내준다.
+        4. 만약 음수 운석 스택에 아무것도 없다면 양수 운석 스택에 현재 운석을 모아준다. 만약 크기가 같은 운석이면 음수 스택에서 해당 운석을 pop 해준다.
+        5. 2 - 5를 반복한다.
+        6. 음수 운석에 
+        
+        
     */
     public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> posi = new Stack<>();
-        Stack<Integer> nega = new Stack<>(); // -10 -5 -1
+        Stack<Integer> nega = new Stack<>(); // Increasing -10 -5 -1
         
         for(int i = asteroids.length - 1; i >= 0; i--){
             if(asteroids[i] < 0) nega.push(asteroids[i]);
