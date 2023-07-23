3a57ce53bd3616586f1d65360ab1d3b813f458ae
Kyle Kim <kyle.kim0923@gmail.com>
2023-07-22 23:11:22 -0700

    Time: 2 ms (63.12%), Space: 40.8 MB (15.55%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,23 @@
+class Solution {
+    /*
+    
+        Algorithm & DS: Stack
+        Time: O(N), Space: O(N)
+    */
+    public int minAddToMakeValid(String s) {
+        int output = 0;
+        Stack<Character> stack = new Stack<>();
+        
+        for(Character c : s.toCharArray()){
+            if(c == '(') stack.push(c);
+            else {
+                if(stack.isEmpty()) output++;
+                else stack.pop();
+            }
+        }
+        
+        output += stack.size();
+        
+        return output;
+    }
+}
