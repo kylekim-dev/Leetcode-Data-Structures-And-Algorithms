82736fa2c630b6f8398b3eb52bc7b878c2d22928
Kyle Kim <kyle.kim0923@gmail.com>
2023-07-27 15:48:12 -0700

    [Gold IV] Title: 데이터 체커, Time: 848 ms, Memory: 63536 KB -BaekjoonHub

--- 백준/Gold/22942. 데이터 체커/데이터 체커.java
+++ 백준/Gold/22942. 데이터 체커/데이터 체커.java
@@ -1,7 +1,11 @@
 import java.io.*;
 import java.util.*;
 public class Main {
+    /*
+        Algorithm & DS: DP, Sort
+        Time: O(N*LogN), Space: O(1)
 
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,22 +18,41 @@ public class Main {
             arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
+        // x, r을 1차원 그래프의 좌표로 변환
+        // EX: [3, 1] => [2, 4]
+        // O(N)
         for(int i = 0; i < N; i++){
             int x = arr[i][0], r = arr[i][1];
             arr[i][0] = x - r;
             arr[i][1] = x + r;
         }
 
+        // 변환된 1차원 그래프를 [a, b]라고하면 a를 기준으로 오름차순 정렬
+        // O(N*LogN)
         Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
 
+        // 교차점이 있는지 확인
+        /*
+           (s1)       (e1)
+            2          8
+            |----------|
+                |-----------|
+                5           11
+               (s2)         (e2)
+
+           위 그림은 교차하는 케이스이다.
+           만약, e1 >= s2 && e1 <= e2 (8 >= 5 && 8 <= 11)이면 교차하는 것이기때문에 No를 반환
+           O(N)
+         */
         for(int i = 1; i < N; i++){
-            int s1 = arr[i - 1][0], s2 = arr[i][0];
-            int e1 = arr[i - 1][1], e2 = arr[i][1];
-            if(s1 <= s2 && e1 <= e2 && s2 <= e1){
+            //int s1 = arr[i - 1][0], s2 = arr[i][0];
+            //int e1 = arr[i - 1][1], e2 = arr[i][1];
+            if(arr[i - 1][1] >= arr[i][0] && arr[i - 1][1] <= arr[i][1]){
                 System.out.println("NO");
                 return;
             }
         }
+
         System.out.println("YES");
     }
 }
