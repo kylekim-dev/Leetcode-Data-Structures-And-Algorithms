356d0d4dd43784fe9c562b7a97460e6095133ab7
Kyle Kim <kyle.kim0923@gmail.com>
2023-09-06 11:34:21 -0700

    Time: 0 ms (100%), Space: 39.6 MB (19.26%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,35 +1,7 @@
 class Solution {
     /*
-
-        1번 바꿀 수 있음
-        가장 앞에 작은 숫자와
-        그 뒤에나오는 가장 큰수를 바꿔야됨
-
-        2736
-
-        27936
-        97236
-
-        9937 >> 9973
-
-
-        01234
-        27936
-
-        0   4   2 < 6, prevMaxIndex 4 
-        0   3   2 < 3, prevMaxIndex 4
-        0   2   2 < 9, prevMaxIndex 2
-        2   7   2 < 7, prevMaxIndex 2 
-        swap 0 and 2
-
-        0123
-        9937
-
-        0   3   9 > 7
-        1   3   9 > 7
-        2   3   3 < 7   p = 3
-        2   2 >> swap 2 and 3
-
+        Algorithm & DS: Greedy
+        Time: O(N), Space: O(N)
     */
     public int maximumSwap(int num) {
         char[] digit = Integer.toString(num).toCharArray();
@@ -37,6 +9,8 @@ class Solution {
 
         max[digit.length - 1] = digit.length - 1;
 
+        // num = 2736 
+        // max = 1133 뒤에서부터의 최대값의 인덱스 배열
         for(int i = max.length - 2; i >= 0; i--){
             max[i] = max[i + 1];
             if(digit[i] > digit[max[i]])
@@ -45,6 +19,8 @@ class Solution {
 
         int l = 0, r = 0;
 
+        // 배열의 왼쪽부터 탐색, max의 값과 비교
+        // 만약 현재 숫자가 max의 숫자보다 작은경우 반복문 종료
         for(int i = 0; i < digit.length ; i++){
             if(digit[i] < digit[max[i]]){
                 l = i;
@@ -52,7 +28,7 @@ class Solution {
                 break;
             }
         }
-
+        // Swap
         char temp = digit[r];
         digit[r] = digit[l];
         digit[l] = temp;
