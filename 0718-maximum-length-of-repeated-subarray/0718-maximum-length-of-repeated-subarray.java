6ea35e589c33906e8d1431fa0930f813d11c85f7
Kyle Kim <kyle.kim0923@gmail.com>
2023-09-18 16:37:45 -0700

    Time: 1564 ms (5.05%), Space: 43 MB (92.45%) - LeetHub

---
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findLength(int[] nums1, int[] nums2) {
+        int max = 0;
+
+        for(int i = 0; i < nums1.length; i++){
+            for(int j = 0; j < nums2.length; j++){
+                if(nums1[i] == nums2[j]) {
+                    int localMax = 0;
+
+                    for(int k = 0; k < nums1.length - i && k < nums2.length - j; k++){
+                        if(nums1[i + k] != nums2[j + k])
+                            break;
+                        localMax++;
+                    }
+
+                    if(max < localMax) max = localMax;
+                }
+            }
+        }
+        
+        return max;
+    }
+}
