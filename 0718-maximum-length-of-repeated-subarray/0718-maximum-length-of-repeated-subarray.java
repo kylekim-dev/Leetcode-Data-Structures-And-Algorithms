ca5fed68372576df6d2e1f1e93bc334e5341da31
Kyle Kim <kyle.kim0923@gmail.com>
2023-09-19 16:07:29 -0700

    Time: 32 ms (32.08%), Space: 54.6 MB (42.35%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.java
@@ -4,21 +4,18 @@ class Solution {
         Time: O(N^3), Space: O(1)
     */
     public int findLength(int[] nums1, int[] nums2) {
+        int[][] dp = new int[nums1.length][nums2.length];
         int max = 0;
 
-        for(int i = 0; i < nums1.length; i++){
-            for(int j = 0; j < nums2.length; j++){
-                if(nums1[i] == nums2[j]) {
-                    int localMax = 0;
-
-                    for(int k = 0; k < nums1.length - i && k < nums2.length - j; k++){
-                        if(nums1[i + k] != nums2[j + k])
-                            break;
-                        localMax++;
-                    }
-
-                    if(max < localMax) max = localMax;
+        for(int i1 = 0; i1 < dp.length; i1++){
+            for(int i2 = 0; i2 < dp[i1].length; i2++){
+                if(nums1[i1] == nums2[i2]){
+                    if(i1 - 1 >= 0 && i2 - 1 >= 0)
+                        dp[i1][i2] = dp[i1 - 1][i2 - 1];
+                    dp[i1][i2]++;
                 }
+
+                if(max < dp[i1][i2]) max = dp[i1][i2];
             }
         }
         
