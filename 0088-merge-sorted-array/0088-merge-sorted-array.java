33d73ede35dc654471fbf0ca89e13746462a9459
Kyle Kim <kyle.kim0923@gmail.com>
2023-12-06 09:28:01 -0800

    Time: 0 ms (100.00%), Space: 41.6 MB (20.09%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -7,11 +7,15 @@ class Solution {
         int n2 = n - 1;
         int n3 = m + n -1;
 
+        // 모든 배열을 뒤에서부터 크기를 비교한다.
+        // 비교해서 더 큰 수를 nums1의 가장자리부터 위치시켜준다.
         while (n1 >= 0 && n2 >= 0){
             if(nums1[n1] >= nums2[n2]) nums1[n3--] = nums1[n1--];
             else nums1[n3--] = nums2[n2--];
         }
 
+        // 하나의 배열을 다 탐색 한 후에 남아있는 데이터들은
+        // nums1에 위치시켜준다.
         while (n1 >= 0) nums1[n3--] = nums1[n1--];
         while (n2 >= 0) nums1[n3--] = nums2[n2--];   
     }
