920f653185ad02dcf09390ebfd32fed5e74c4012
Kyle Kim <kyle.kim0923@gmail.com>
2023-12-07 10:04:39 -0800

    Time: 1946 ms (5.00%), Space: 54.4 MB (5.48%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,59 +1,36 @@
-import java.util.*;
-import java.util.stream.*;
-
 class Solution {
-    Set<NumsClass> answer = new HashSet<>();
-    Map<Integer, Integer> map = new HashMap<>();
+    /*
+        Algorithm & DS: HashSet & Iterator
+        Time: O(N^2)
+        Space: O(M)
+    */
     public List<List<Integer>> threeSum(int[] nums) {
-        for(int i : nums) map.put(i, map.getOrDefault(i, 0) + 1);
-        if(map.getOrDefault(0, 0) >= 3) answer.add(new NumsClass(0, 0, 0));
-        for(int n1 : map.keySet()) {
-            for(int n2 : map.keySet()) {
-                if(n1 == n2) continue;
-                addIfSumZero(n1, n2);
+        List<List<Integer>> result = new ArrayList<>();
+        HashSet<String> duplicateChecker = new HashSet<>();
+        
+        // 첫번째 값을 기준으로 탐색시작
+        for(int i = 0; i < nums.length - 2; i++){
+            HashSet<Integer> finder = new HashSet<>();
+            
+            for(int j = i + 1; j < nums.length; j++){
+                int c = -(nums[i] + nums[j]); 
+                
+                if(finder.contains(c)){
+                    List<Integer> arr = Arrays.asList(nums[i], nums[j], c);
+                    Collections.sort(arr);
+                    
+                    String key = arr.toString();
+                    
+                    if(!duplicateChecker.contains(key)){
+                       duplicateChecker.add(key);
+                       result.add(arr);
+                    }
+                }
+                
+                finder.add(nums[j]);
             }
         }
-        return answer.stream().map(NumsClass::getList).collect(Collectors.toList());
-    }
-    public void addIfSumZero(int n1, int n2) {
-        for(int i = 1 ; i <= map.get(n1) ; i++) {
-            for(int j = 1 ; j <= map.get(n2) ; j++) {
-                if(i + j == 3 && n1 * i + n2 * j == 0) addHaveSame(n1, n2, i, j);
-                if(i + j == 2 && map.containsKey(-n1 - n2)) addAllDiff(n1, n2);
-            }
-        }
-    }
-    public void addHaveSame(int n1, int n2, int idx1, int idx2) {
-        NumsClass list = new NumsClass();
-        for(int k = 0 ; k < idx1 ; k++) list.getList().add(n1);
-        for(int k = 0 ; k < idx2 ; k++) list.getList().add(n2);
-        answer.add(list);
-    }
-    public void addAllDiff(int n1, int n2) {
-        if(n1 * 2 + n2 == 0 || n2 * 2 + n1 == 0) return;
-        answer.add(new NumsClass(n1, n2, -n1 - n2));
-    }
-}
-
-class NumsClass {
-    private List<Integer> list;
-    public List<Integer> getList() {
-        return list;
-    }
-    public NumsClass() {
-        this.list = new ArrayList<>();
-    }
-    public NumsClass(int n1, int n2, int n3) {
-        this.list = Arrays.asList(n1, n2, n3);
-    }
-    @Override
-    public boolean equals(Object o) {
-        NumsClass obj = (NumsClass) o;
-        return this.list.equals(obj.list);
-    }
-    @Override
-    public int hashCode() {
-        Collections.sort(list);
-        return list.hashCode();
+        
+        return result;
     }
 }
