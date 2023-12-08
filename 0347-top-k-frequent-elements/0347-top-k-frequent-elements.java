d8e4a4aa0f39687d796f163d83e775aff72ca524
Kyle Kim <kyle.kim0923@gmail.com>
2023-12-08 11:34:34 -0800

    Time: 13 ms (70.45%), Space: 47.6 MB (57.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,33 +1,23 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer, Integer> count = new HashMap<>();
-        int max = Integer.MIN_VALUE;
-        int[] output = new int[k];
-        
-        for(int num : nums){
-            count.put(num, count.getOrDefault(num, 0) + 1); //{ 1:3, 2:2, 3:1 }
-            max = Math.max(max, count.get(num));
-        } 
-        
-        List<List<Integer>> bucketSort = new ArrayList<>();
-        
-        for(int i = 0; i <= max; i++) bucketSort.add(new ArrayList<>());
-        
-        count.forEach((key, value) -> {
-            List<Integer> list = bucketSort.get(value);
-            list.add(key);
-        });
-        
-        
-        for(int i = bucketSort.size() - 1; i >= 0; i--){
-            List<Integer> list = bucketSort.get(i);
-            
-            for(int num : list){
-                output[--k] = num;
-                if(k <= 0) return output;
-            }
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        for(int n : nums){
+            countMap.put(n, countMap.getOrDefault(n, 0) + 1);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> countMap.get(o2).compareTo(countMap.get(o1)));
+
+        for (Integer  n : countMap.keySet()){
+            pq.offer(n);
+        }
+
+        int[] res = new int[k];
+
+        for(int i = 0; i < k; i++){
+            res[i] = pq.poll();
         }
         
-        return output;
+        return res;
     }
 }
