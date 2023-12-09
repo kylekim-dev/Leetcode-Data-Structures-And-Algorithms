b749e30f01836ea2f1358e182308358d5b3d7125
Kyle Kim <kyle.kim0923@gmail.com>
2023-12-08 16:15:44 -0800

    Time: 7 ms (10.15%), Space: 43.9 MB (85.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,46 +1,34 @@
 class Solution {
-    /*
-        height[]   = 0 1 0 2 1 0 1 3 2 1 2 1
-        rightMax[] = 2 2 2 2 3 3 3 3 2 2 2 1
-        leftMax[]  = 0 1 1 2 2 2 2 3 3 3 3 3
-        
-        
-        0 0 1 0 1 2 1 0 0 1 0 0 
-        0 0 1 0 1 2 1 0 0 1 0 0
-        
-        아이디어
-            오른쪽 시점에서 바라봤을때 가장 높은 기둥 값을 저장하는 rightMax[]
-            왼쪽 시점에서 바라봤을때 가장 높은 기둥 값을 저장하는 leftMax[]
-            sum += Math.Min(rightMax[i], leftMax[i]) - height[i]
-            
-        알고리즘
-            투포인터
-            
-        시간복잡도
-            T: O(N)
-            S: O(N)
-    */
     public int trap(int[] height) {
-        int[] leftMax = new int[height.length];
-        int[] rightMax = new int[height.length];
-        int sum = 0;
-        int r = height.length - 1;
+        // Algorithm & DS: Monotonic Stack
+        // Time: O(n), Extra Space: O(n)
 
-        leftMax[0] = height[0];
-        rightMax[r] = height[r];
-        
-        for(int l = 1; l < height.length; l++){
-            r -= 1;
-            leftMax[l] = Math.max(leftMax[l - 1], height[l]);
-            rightMax[r] = Math.max(rightMax[r + 1], height[r]);
+        Stack<Integer> monoStack = new Stack<>();
+        int area = 0;
+
+        for(int r = 0; r < height.length; r++){
+            int prevMaxHeight = 0;
+
+            while (!monoStack.isEmpty() && height[monoStack.peek()] <= height[r]){
+                int l = monoStack.pop();
+                int x = r - l - 1;
+                int y = Math.min(height[l], height[r]);
+
+                area += x * (y - prevMaxHeight);
+
+                prevMaxHeight = height[l];
+            }
+
+            if(!monoStack.isEmpty()){
+                int l = monoStack.peek();
+                int x = r - l - 1;
+                int y = Math.min(height[l], height[r]);
+                area += x * (y - prevMaxHeight);
+            }
+
+            monoStack.push(r);
         }
         
-        
-        for(int i = 0; i < height.length; i++){
-            sum += Math.min(leftMax[i], rightMax[i]) - height[i];
-        }
-        
-        
-        return sum;
+        return area;
     }
 }
