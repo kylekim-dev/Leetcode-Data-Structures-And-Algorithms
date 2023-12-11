bacd901c7124675a5aae44d8cfa8cee6dda6c50a
Kyle Kim <kyle.kim0923@gmail.com>
2023-12-11 12:59:41 -0800

    Time: 7 ms (82.33%), Space: 41.7 MB (57.99%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,45 +1,34 @@
 class Solution {
-    /*
-        AABABBA, K = 2
-        
-        l   r   lFreq   rFreq   diff    maxFrq
-        0   0   1       1       0       1
-        0   1   2       2       0       2
-        0   2   2       1       1(A)    3
-        0   3   3       1       1(A)    4
-        0   4   3       2       2(A)    5
-        0   5   3       3       3(A,B)  x
-        1   5   2       3       2(A)    5
-        1   6   3       3       3(A,B)  x
-        2   6   2       3       2(B)    5
-    
-    */
     public int characterReplacement(String s, int k) {
-        int[] freq = new int[26];
+        // Algorithm & DS: Sliding Window
+        // Time: O(n), Extra Space: O(1)
+        int longest = 0;
         int l = 0, r = 0;
-        int lKey, rKey, len;
-        int most = 0, maxFreq = 0;
-        
-        l = r = 0;
+        int[] arr = new int[26];
+        int mostIdx = 0;
 
         while (r < s.length()){
-            lKey = s.charAt(r) - 'A';
-            freq[lKey] += 1;
-            most = Math.max(most, freq[lKey]);
-            len = r - l + 1;
-
-            if(len - most > k){
-                rKey = s.charAt(l) - 'A';
-                freq[rKey] -= 1;
-                freq[lKey] -= 1;
+            int currAlpha = s.charAt(r) - 'A';
+            arr[currAlpha] += 1;
+            // most index
+            if(arr[mostIdx] < arr[currAlpha]){
+                mostIdx = currAlpha;
+            }
+            
+            while (r - l + 1 > k + arr[mostIdx]){
+                int leftAlpha = s.charAt(l) - 'A';
+                arr[leftAlpha] -= 1;
                 l += 1;
-            }
-            else {
-                maxFreq = Math.max(maxFreq, len);
-                r += 1;
-            }
-        }
 
-        return maxFreq;
+                if(leftAlpha == mostIdx){
+                    break;
+                }
+            }
+
+            longest = Math.max(longest, r - l + 1);
+            r++;
+        }
+        
+        return longest;
     }
 }
