df215c1dc8e0b80f7c626271849b31aecd26caec
Kyle Kim <kyle.kim0923@gmail.com>
2023-12-18 16:41:49 -0800

    Time: 3 ms (13.08%), Space: 40.6 MB (41.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean isValid(String s) {
-        // Algorithm: Stack, Iterator
-        // Time: O(N), Space: O(N)
         Stack<Character> stack = new Stack<>();
-        
+
         for(Character c : s.toCharArray()){
-            if (c == '(') stack.push(')');
-            else if (c == '[') stack.push(']');
-            else if (c == '{') stack.push('}');
-            else if (stack.isEmpty() || stack.pop() != c) return false;
+            if(stack.isEmpty() || c == '(' || c == '[' || c == '{') stack.push(c);
+            else if(stack.peek() == '(' && c == ')') stack.pop();
+            else if(stack.peek() == '[' && c == ']') stack.pop();
+            else if(stack.peek() == '{' && c == '}') stack.pop();
+            else return false;
         }
         
         return stack.isEmpty();
