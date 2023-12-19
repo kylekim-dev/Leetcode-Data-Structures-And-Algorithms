dea6029d95550d8a84e7a32160992f21ef471340
Kyle Kim <kyle.kim0923@gmail.com>
2023-12-19 14:55:32 -0800

    Time: 58 ms (72.75%), Space: 55.4 MB (71.78%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,27 +1,18 @@
 class Solution {
-    /*
-        디크리징 스택
-         0  1  2  3  4   5  6 7
-        [73,74,75,71,69,72,76,73]
-        [4 2 1 1 0 0]
-        74 75 76
-    
-        stack이 존재하지 않으면 본인이 제일 큰 것이므로 0
-        stack이 존재하면 peek의 인덱스 j와 현재 인덱스 i를 뺀 j - i를 넣어준다.
-    */
     public int[] dailyTemperatures(int[] temperatures) {
-        Stack<Integer> stack = new Stack<>();
-        int[] ans = new int[temperatures.length];
-        
-        for(int i = temperatures.length - 1; i >= 0; i--){
-            while(!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]){
-                stack.pop();
+        int[] answer = new int[temperatures.length];
+
+        Stack<Integer> monoStack = new Stack<>(); // 내림차순 모노토닉 스택
+
+        for(int i = 0; i < temperatures.length; i++){
+            while (!monoStack.isEmpty() &&  temperatures[monoStack.peek()] < temperatures[i]){
+                int day = monoStack.pop();
+                answer[day] = i - day;
             }
-            
-            if(!stack.isEmpty()) ans[i] = stack.peek() - i;
-            stack.push(i);
+
+            monoStack.push(i);
         }
-        
-        return ans;
+     
+        return answer;
     }
 }
