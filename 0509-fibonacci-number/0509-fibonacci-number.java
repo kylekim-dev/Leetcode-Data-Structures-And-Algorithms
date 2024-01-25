482dab9b18a5aa4cfd51463bbf51e858fb062ae9
Kyle Kim <kyle.kim0923@gmail.com>
2024-01-24 16:11:23 -0800

    Time: 0 ms (100.00%), Space: 39.9 MB (64.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,19 @@
 class Solution {
     public int fib(int n) {
-        if(n == 1) return 1;
-        else if(n < 1) return 0;
+        /*
+            Data Structure & Algorithms: DP
+            Time: O(N), extra space: O(N)
+        */
         
-        return fib(n - 1) + fib(n - 2);
+        int[] dp = new int[n + 1];
+        
+        if(n >= 1) dp[1] = 1;
+        if(n >= 2) dp[2] = 1;
+        
+        for(int i = 3; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
     }
 }
