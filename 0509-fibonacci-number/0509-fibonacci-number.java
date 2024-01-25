ced533872521bdc3dfab77d6226d5161e7e84870
Kyle Kim <kyle.kim0923@gmail.com>
2024-01-24 16:17:30 -0800

    Time: 0 ms (100.00%), Space: 40.3 MB (29.55%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -5,25 +5,15 @@ class Solution {
             Time: O(N), extra space: O(N)
         */
         
-        /*int[] dp = new int[n + 1];
+        int[] dp = new int[n + 1];
         
         if(n >= 1) dp[1] = 1;
         if(n >= 2) dp[2] = 1;
         
         for(int i = 3; i <= n; i++){
             dp[i] = dp[i-1] + dp[i-2];
-        }*/
-        
-        int prev = 0, answer = 0;
-        
-        if(n > 0) answer = 1;
-        
-        for(int i = 2; i <= n; i++){
-            int temp = answer;
-            answer = answer + prev;
-            prev = temp;
         }
         
-        return answer;
+        return dp[n];
     }
 }
