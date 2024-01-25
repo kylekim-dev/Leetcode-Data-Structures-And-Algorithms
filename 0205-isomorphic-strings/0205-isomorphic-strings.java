900a86c75e57fa9c7ead5d4d83c3e40c8b6b777d
Kyle Kim <kyle.kim0923@gmail.com>
2024-01-25 14:20:47 -0800

    Time: 5 ms (97.31%), Space: 42.3 MB (42.93%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -5,25 +5,16 @@ class Solution {
          * Time: O(N), Extra Space: O(M)
          */
 
-        int[] map1 = new int[128];
-        int[] map2 = new int[128];
+        int[] map1 = new int[128], map2 = new int[128];
 
         for(int i = 0; i < s.length(); i++){
             int s1 = s.charAt(i), t1 = t.charAt(i);
 
-            if(map1[s1] == 0){
+            if(map1[s1] == 0 && map2[t1] == 0){
                 map1[s1] = t1;
-            }
-
-            if(map1[s1] != t1){
-                return false;
-            }
-
-            if(map2[t1] == 0){
                 map2[t1] = s1;
             }
-
-            if(map2[t1] != s1){
+            else if(map1[s1] != t1 || map2[t1] != s1){
                 return false;
             }
         }
