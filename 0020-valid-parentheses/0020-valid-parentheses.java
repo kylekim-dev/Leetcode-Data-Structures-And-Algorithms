adfe3a58226e5a6b260915f0e157f6bc16cc35d8
Kyle Kim <kyle.kim0923@gmail.com>
2024-01-29 17:32:11 -0800

    Time: 2 ms (67.50%), Space: 41.6 MB (27.04%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -3,14 +3,12 @@ class Solution {
         /*
             Algorithms && DS: Stack
             Time: O(N), Space: O(N)
-            
         */
         Stack<Character> stack = new Stack<>();
 
         for(Character c : s.toCharArray()){
             if(stack.isEmpty() || c == '(' || c == '[' || c == '{') stack.push(c);
             else if(stack.peek() == '(' && c == ')' || stack.peek() == '[' && c == ']' || stack.peek() == '{' && c == '}') stack.pop();
-            else if(stack.isEmpty() && (c == ')' || c == ']' || c == '}')) return false;
             else return false;
         }
         
