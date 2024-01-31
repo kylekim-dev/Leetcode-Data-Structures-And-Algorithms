8d0c6366289e3d47743da709814c710d7093cb82
Kyle Kim <kyle.kim0923@gmail.com>
2024-01-31 14:53:37 -0800

    Time: 4 ms (54.58%), Space: 44.4 MB (17.50%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,51 +1,34 @@
+import java.util.*;
+
 class Solution {
-    /*
-        
-        
-        // Algorithm: Monotonic-Stack
-        // Time: O(N), Space: O(N)
-        
-        [4,3,9]
-        [1,5,4,3,2,9]
-    */
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        /*
+            Algorithms & DS: Monotonic Stack
+            Time: O(M + N), Extra Space: O(N)
+            M = nums1.length
+            N = nums2.length
+         */
+
         int[] ans = new int[nums1.length];
         Stack<Integer> stack = new Stack<>();
         HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(int j = 0; j < nums2.length; j++){
-            while(!stack.isEmpty() && stack.peek() < nums2[j])
-                map.put(stack.pop(), nums2[j]);
-            
-            stack.push(nums2[j]);
+
+        for(int n2 : nums2){
+            while (!stack.isEmpty() && stack.peek() < n2){
+                int smaller = stack.pop();
+
+                if(!map.containsKey(smaller)){
+                    map.put(smaller, n2);
+                }
+            }
+
+            stack.push(n2);
         }
 
         for(int i = 0; i < nums1.length; i++){
             ans[i] = map.getOrDefault(nums1[i], -1);
         }
-        
+
         return ans;
     }
-    
-    /*
-    // Algorithm: Brute-Force 
-    // Time: O(N*M), Space: O(N)
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        
-        for(int i = 0; i < nums1.length; i++){
-            boolean isSame = false;
-            ans[i] = -1;
-            
-            for(int j = 0; j < nums2.length; j++){
-                if(nums1[i] == nums2[j]) isSame = true;
-                if(isSame && nums1[i] < nums2[j]) {
-                    ans[i] = nums2[j];
-                    break;
-                } 
-            }
-        }
-        
-        return ans;
-    }*/
 }
