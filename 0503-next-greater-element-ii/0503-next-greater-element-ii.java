68439c4d04f796580e9124011eeb26cc71748548
Kyle Kim <kyle.kim0923@gmail.com>
2024-02-02 19:37:23 -0800

    Time: 14 ms (70.69%), Space: 46.2 MB (31.08%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,28 +1,23 @@
 class Solution {
-    // Algorithm: Monotonic Stack (Forward)
-    // Time: O(N), Space: O(N)
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
-        int[] output = new int[n];
-        Stack<Integer> stack = new Stack<>();
-        
-        for(int i = 0; i < n; i++){
-            while(!stack.isEmpty() && nums[stack.peek()] < nums[i]){
-                output[stack.pop()] = nums[i];
-            }
+        /*
+            Algorithms & DS: Monotonic stack
+            Time: O(N), Extra Space: O(N)
+        */
+        Stack<Integer> monoStack = new Stack<>(); 
+        int[] output = new int[nums.length];
 
-            output[i] = -1;
-            stack.push(i);
-        }
-        
-        for(int i = 0; i < n; i++){
-            while(!stack.isEmpty() && nums[stack.peek()] < nums[i]){
-                output[stack.pop()] = nums[i];
-            }
+        Arrays.fill(output, -1);
+        for(int c = 0; c < 2; c++){
+            for(int i = 0; i < nums.length; i++){
+                while(!monoStack.isEmpty() && nums[monoStack.peek()] < nums[i]){
+                    output[monoStack.pop()] = nums[i];
+                }
 
-            stack.push(i);
+                monoStack.push(i);
+            }
         }
-        
+
         return output;
     }
 }
