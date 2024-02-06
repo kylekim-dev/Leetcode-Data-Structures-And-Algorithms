1a2c4c57a5f70e919233514f9a5970bc3529fe1c
Kyle Kim <kyle.kim0923@gmail.com>
2024-02-06 14:24:59 -0800

    Time: 0 ms (100.00%), Space: 40.9 MB (47.07%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,14 +14,11 @@
  * }
  */
 class Solution {
-    private boolean res = true;
     public boolean isSameTree(TreeNode p, TreeNode q) {
+
         if(p == null && q == null) return true;
         else if(p == null || q == null || p.val != q.val) return false;
         
-        res = res && isSameTree(p.left, q.left);
-        res = res && isSameTree(p.right, q.right);
-        
-        return res;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
 }
