4b8c3b450a271fc8e0e5bebf9a61ab00d9b1e4fa
Kyle Kim <kyle.kim0923@gmail.com>
2024-02-21 16:33:01 -0800

    Time: 0 ms (100.00%), Space: 42.1 MB (65.13%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,6 +1,5 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
         /*
             Algorithms & DS: #Two Pointer
             Time: O(m + n), Extra Space: O(1)
@@ -8,16 +7,10 @@ class Solution {
         m -= 1; n -= 1;
 
         while (m >= 0 && n >= 0){
-            if(nums1[m] < nums2[n])  {
-                nums1[m + n + 1] = nums2[n--];
-            }
-            else {
-                nums1[m + n + 1] = nums1[m--];
-            }
+            if(nums1[m] < nums2[n]) nums1[m + n + 1] = nums2[n--];
+            else nums1[m + n + 1] = nums1[m--];
         }
 
-        while (n >= 0){
-            nums1[n] = nums2[n--];
-        }
+        while (n >= 0) nums1[n] = nums2[n--];
     }
 }
