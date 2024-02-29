c2a160b8e22b42cfa2fc2eeacbb4ff85f4f9a2a3
Kyle Kim <kyle.kim0923@gmail.com>
2024-02-28 20:57:11 -0800

    Time: 7 ms (84.85%), Space: 42.2 MB (87.98%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,34 +1,36 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        // Algorithm & DS: Sliding Window
-        // Time: O(n), Extra Space: O(1)
-        int longest = 0;
+        /*
+         * Algorithms & DS: #Sliding Window
+         * Time: O(N), Extra Space: O(1)
+         */
+
+        int[] alphabetCount = new int[26];
+        int max = 0;
         int l = 0, r = 0;
-        int[] arr = new int[26];
-        int mostIdx = 0;
+        int mostAlpha = 0;
 
-        while (r < s.length()){
-            int currAlpha = s.charAt(r) - 'A';
-            arr[currAlpha] += 1;
-            // most index
-            if(arr[mostIdx] < arr[currAlpha]){
-                mostIdx = currAlpha;
+        while (r < s.length()) {
+            int rAlpha = s.charAt(r) - 'A';
+            alphabetCount[rAlpha]++;
+
+            if(alphabetCount[mostAlpha] < alphabetCount[rAlpha]){
+                mostAlpha = rAlpha;
             }
-            
-            while (r - l + 1 > k + arr[mostIdx]){
-                int leftAlpha = s.charAt(l) - 'A';
-                arr[leftAlpha] -= 1;
-                l += 1;
 
-                if(leftAlpha == mostIdx){
+            while (r - l + 1 > k + alphabetCount[mostAlpha]) {
+                int lAlpha = s.charAt(l) - 'A';
+                alphabetCount[lAlpha]--;
+                l++;
+                if(lAlpha == mostAlpha){
                     break;
                 }
             }
 
-            longest = Math.max(longest, r - l + 1);
+            max = Math.max(max, r - l + 1);
             r++;
         }
         
-        return longest;
+        return max;
     }
 }
