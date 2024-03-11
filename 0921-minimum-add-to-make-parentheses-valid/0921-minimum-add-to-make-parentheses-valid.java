d983c83b03db35f6318cf8df1cc0368ca335e848
Kyle Kim <kyle.kim0923@gmail.com>
2024-03-10 21:35:34 -0700

    Time: 0 ms (100.00%), Space: 41.2 MB (87.76%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -1,16 +1,29 @@
 class Solution {
-    /*
-        Algorithm & DS: Iterator
-        Time: O(N), Space: O(1)
-    */
     public int minAddToMakeValid(String s) {
-        int output = 0, openerCount = 0;
-        
-        for(Character c : s.toCharArray()){
-            if(c == ')' && openerCount == 0) output++;
-            else openerCount += (c == '(') ? 1 : -1;
+        /*
+         * Algorithms & DS: #Greedy
+         * Time: O(N), Extra Space: O(1)
+         */
+
+        int bal = 0;
+        int ans = 0;
+
+        for(char c : s.toCharArray()){
+            if(c == ')'){
+                if(bal > 0){
+                    bal--;
+                }
+                else {
+                    ans++;
+                }
+            }
+            else {
+                bal++;
+            }
         }
+
+        ans += bal;
         
-        return output + openerCount;
+        return ans;
     }
 }
