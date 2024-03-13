0d06912ad74d8461179fb244d035c7b1db63b26a
Kyle Kim <kyle.kim0923@gmail.com>
2024-03-12 22:20:29 -0700

    Time: 0 ms (100.00%), Space: 40.1 MB (82.85%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,24 +1,20 @@
 class Solution {
-    /*
-        n = 1, // 1
-        n = 2, // 2
-        n = 3, // 3
-        n = 4, // 5
-        n = 5, // 8
-        n = 6, // 13
-
-        피보나치 규칙
-        
-    */
     public int climbStairs(int n) {
-        // Algorithm: DP
-        // Time: O(N), Space: O(1)
-        int prev = 0, curr = 1;
+        /*
+         * Algorithms & DS: #DP
+         * Time: O(N), Extra Space: O(1)
+            1 = 1
+            2 = 2
+            3 = 3
+            4 = 5
+            5 = 8
+        */
+        int p = 1, curr = 1;
         
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i < n; i++){
             int temp = curr;
-            curr += prev;
-            prev = temp;
+            curr = curr + p;
+            p = temp;
         }
         
         return curr;
