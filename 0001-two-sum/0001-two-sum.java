97b640b8c4bf8e3d1e104fecc70296a895f11051
Kyle Kim <kyle.kim0923@gmail.com>
2024-04-02 16:00:58 -0700

    Time: 2 ms (97.80%), Space: 44.8 MB (42.32%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,29 +1,21 @@
 class Solution {
-    /*
-        Time: 6m 42s
-        
-        아이디어
-            - A + B = Target
-            - 해쉬 테이블 사용
-            - Map<B, index of A>
-            
-        시간복잡도
-            T: O(N)
-            S: O(N)
-            
-        자료구조
-            해쉬 맵
-    
-    */
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i])) 
-                return new int[]{ map.get(nums[i]), i};
-            
-            map.put(target - nums[i], i);
-        }
+        /*
+            Algorithms & DS: #Hashing
+            Time: O(N), Space: O(N)
+         */
+
+         HashMap<Integer, Integer> map = new HashMap<>();
+
+         for(int i = 0; i < nums.length; i++){
+             int curr = nums[i];
+             int wanted = target - curr;
+             if(map.containsKey(curr)){
+                 return new int[]{ map.get(curr), i};
+             }
+
+             map.put(wanted, i);
+         }
         
         return null;
     }
