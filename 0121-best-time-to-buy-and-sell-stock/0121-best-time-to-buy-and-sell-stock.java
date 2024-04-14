79cbc485654903a846e212f62b1600c2f6ff454c
Kyle Kim <kyle.kim0923@gmail.com>
2024-04-13 23:08:30 -0700

    Time: 2 ms (78.89%), Space: 61.3 MB (77.13%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,16 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        // Algorithm & DS: Sliding Window
-        // Time: O(n), Extra Space: O(1)
-        
+        /*
+         * Algorithms & DS: #DP
+         * Time: O(N), Extra Space: O(1)
+         */
+
         int maxProfit = 0;
         int minPrice = prices[0];
 
-        for(int i = 1; i < prices.length; i++){
-            minPrice = Math.min(minPrice, prices[i]);
-            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        for(int currPrice : prices){
+            maxProfit = Math.max(currPrice - minPrice, maxProfit);
+            minPrice = Math.min(minPrice, currPrice); 
         }
         
         return maxProfit;
