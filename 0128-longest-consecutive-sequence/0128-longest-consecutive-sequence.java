b3a7b94bcece5a579972b89654b27fcf82f55d78
Kyle Kim <kyle.kim0923@gmail.com>
2024-04-15 19:04:45 -0700

    Time: 81 ms (36.01%), Space: 72.7 MB (5.31%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,25 +1,51 @@
 class Solution {
-    public int longestConsecutive(int[] nums) {
-        // Algorithm & DS: HashMap
-        // Time: O(n), Extra Space: O(n)
-        HashSet<Integer> setNum = new HashSet<>();
-
-        for (int n : nums)
-            setNum.add(n);
-
-        int longest = 0;
-
-        for (int n : nums){
-            if(!setNum.contains(n - 1)){
-                int i = 0;
-
-                while (setNum.contains(n + i))
-                    i++;
-
-                longest = Math.max(longest, i);
-            }
+    public int findRoot(int num, HashMap<Integer, Integer> parent){
+        if(!parent.containsKey(num)){
+            return num;
         }
 
-        return longest;
+        if(parent.get(num) != num){
+            return findRoot(parent.get(num), parent);
+        }
+
+        return parent.get(num);
+    }
+    
+    public int longestConsecutive(int[] nums) {
+        /*
+            Algorithms & DS: #HashMap, #Union Find
+            Time: O(N), Space: O(N)
+         */
+
+        int ans = 0;
+        HashMap<Integer, Integer> parent = new HashMap<>();
+        HashMap<Integer, Integer> count = new HashMap<>();
+
+        for(int num : nums){
+            if(parent.containsKey(num))
+                continue;
+
+            int rootL = findRoot(num - 1, parent);
+            int rootR = findRoot(num + 1, parent);
+
+            int c = 1;
+
+            if(count.containsKey(rootL)){
+                parent.put(rootL, num);
+                c += count.get(rootL);
+            }
+
+            if(count.containsKey(rootR)){
+                parent.put(rootR, num);
+                c += count.get(rootR);
+            }
+
+            parent.put(num, num);
+            count.put(num, c);
+
+            ans = Math.max(ans, c);
+        }
+        
+        return ans;
     }
 }
