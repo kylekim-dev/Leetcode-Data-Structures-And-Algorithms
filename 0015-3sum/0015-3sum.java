38e94deeccec6f2a8e28e8e0a383c8fe381016ef
Kyle Kim <kyle.kim0923@gmail.com>
2024-04-15 21:48:05 -0700

    Time: 27 ms (90.84%), Space: 51.2 MB (76.48%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,31 @@
 class Solution {
+    // Algorithm & DS: Two Pointers
+    // Time: O(n^2), Extra Space: O(1)
+    
     public List<List<Integer>> threeSum(int[] nums) {
-        /*
-         * Algorithms & DS: #Two Pointer, #HashMap
-         * Time: O(N^2), Extra Space: O(N)
-         */
-        List<List<Integer>> ans = new ArrayList<>();
-        HashSet<String> checker = new HashSet<>();
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums); // O(N*LogN)
 
-        for(int i = 0; i < nums.length - 2; i++){
-            int a = nums[i];
-            HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
 
-            for(int j = i + 1; j < nums.length; j++){
-                int b = nums[j];
-                if(set.contains(-b)){
-                    int[] temp = {a, b, -(a + b)};
-                    Arrays.sort(temp);
-                    String tempStr = Arrays.toString(temp);
+            int l = i + 1, r = nums.length - 1;
 
-                    if(!checker.contains(tempStr)){
-                        checker.add(tempStr);
-                        ans.add(Arrays.asList(temp[0], temp[1], temp[2]));
+            while (l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum > 0) r--;
+                else if (sum < 0) l++;
+                else {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    l += 1;
+                    while (nums[l] == nums[l - 1] && l < r){
+                        l++;
                     }
                 }
-
-                set.add(a + b);
             }
         }
         
-        return ans;
+        return res;
     }
 }
