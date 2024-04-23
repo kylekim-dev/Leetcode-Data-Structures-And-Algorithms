bd78e5e8ada76e4c5cfc0194fcf5746810194c0c
Kyle Kim <kyle.kim0923@gmail.com>
2024-04-23 16:22:19 -0700

    Time: 65 ms (54.81%), Space: 63.1 MB (16.85%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,22 +1,23 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        /*
-            Algorithms & DS: Monotonic Stack
-            Time: O(N), Space: O(N)
-        */
-        int[] answer = new int[temperatures.length];
 
-        Stack<Integer> monoStack = new Stack<>(); // 내림차순 모노토닉 스택
+        /*
+            Algorithms & DS: #Sliding Window, #Monotonic-Stack
+            Time: O(N), Space: O(N)
+         */
+
+        int[] ans = new int[temperatures.length];
+        Stack<Integer> monoStack = new Stack<>();
 
         for(int i = 0; i < temperatures.length; i++){
-            while (!monoStack.isEmpty() &&  temperatures[monoStack.peek()] < temperatures[i]){
-                int day = monoStack.pop();
-                answer[day] = i - day;
+            while (!monoStack.isEmpty() && temperatures[monoStack.peek()] < temperatures[i]){
+                int warmerIdx = monoStack.pop();
+                ans[warmerIdx] = i - warmerIdx;
             }
 
             monoStack.push(i);
         }
-     
-        return answer;
+        
+        return ans;
     }
 }
